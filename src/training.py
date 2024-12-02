3b4e53ee56620794e560511a84402e7e0e3e63e3
Theeravich (Arthur) Trakulkajornsak <arthurtrakul@gmail.com>
2024-12-02 06:18:40 -0500

    Adjusted hyperparameters/file naming scheme
    Additionally commented out GPU allocation for PyTorch since it was inconsistently being used.

--- src/training.py
+++ src/training.py
@@ -1,16 +1,16 @@
 from torch_geometric.loader import DataLoader
-import torch
+import torch, os
 from torch.optim import AdamW
 from torch import nn
 import torch.nn.functional as F
 from sklearn.metrics import f1_score
 
-from data_processing import read_json, get_dataset, get_cause_relations
+from data_processing import read_json, get_dataset, get_cause_relations, TRANSFORMER
 from graph_models import CauseExtractor
 
 
-DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
+DEVICE = torch.device("cpu")
+# DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 def train_model() -> None:
     """
@@ -20,15 +20,20 @@ def train_model() -> None:
     train = read_json(train_file)
     get_cause_relations(train)
     train_dataset = get_dataset(train)
+
     val_file = "data/Subtask_1_dev.json"
     val = read_json(val_file)
     get_cause_relations(val)
     val_dataset = get_dataset(val)
 
-    num_epochs = 300
+
+
+    num_epochs = 200
     batch_size = 32
     learning_rate = 0.0001
     weight_decay = 1e-5
+
+
 
     loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
     val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
@@ -59,13 +64,13 @@ def train_model() -> None:
             metric = f1_score(y_golds, y_preds)
             if metric > best_metric:
                 best_metric = metric
-                torch.save(model.state_dict(), "models/new_best_model.pth")
+                torch.save(model.state_dict(), "models/new_best_model_" + TRANSFORMER + ".pth")
             if epoch % 10 == 0:
                 print(
-                    f"""Epoch {epoch+1}/{num_epochs}, Train Loss: {round(loss.item(), 2)}, Validation F1: {round(metric, 2)}"""
+                    f"""Epoch {epoch+1}/{num_epochs}, Train Loss: {round(loss.item(), 3)}, Validation F1: {round(metric, 3)}"""
                 )
-    print("Best Validation F1:", round(best_metric, 2))
-    model.load_state_dict(torch.load("models/new_best_model.pth"))
+    print("Best Validation F1:", round(best_metric, 3))
+    model.load_state_dict(torch.load("models/new_best_model_" + TRANSFORMER + ".pth"))
 
 
 if __name__ == "__main__":
